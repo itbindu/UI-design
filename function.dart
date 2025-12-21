9c292d1de84705972b611a4cce1a3b1c473ad95a
gugulothu bindu <22wh1a1273@bvrithyderabad.edu.in>
2025-12-21 18:52:36 +0530

    Project

---
+++ function.dart
@@ -0,0 +1,8 @@
+void main() {
+  int result = multiply(4, 5);
+  print('The result is $result');
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
---
+++ sum.dart
@@ -0,0 +1,8 @@
+void main() {
+  int num1 = 5;
+  int num2 = 7;
+
+  int sum = num1 + num2;
+
+  print('The sum of $num1 and $num2 is $sum.');
+}
